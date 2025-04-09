c0efb0d56d116167d807214c270e870794ec3b19
Richard <57541025+richardh05@users.noreply.github.com>
2025-04-09 20:17:24 +0100

    connectDb; slight refactor

--- python/ExtractMd.py
+++ python/ExtractMd.py
@@ -1,29 +1,40 @@
 import MarkdownParser
 from typing import List
-from io import StringIO
 import pandas
+import sqlite3
 
-def extractMD(path: str) -> List[str]:
+SQLitePath:str = "/home/richard/Documents/gym"
+
+def connectDb() -> sqlite3.Connection:
+    try:
+        conn = sqlite3.connect(SQLitePath)
+        return conn
+    except sqlite3.Error as e:
+        print(f"Error connecting to the database: {e}")
+    return None
+
+def writeDb(f):
     mp = MarkdownParser.MarkdownParser()
+    days = mp.seperateByHeader(f.readlines(),"# ")
+    for day in days:
+        date = mp.getFirstLine(day,"# ")
+        exercises = mp.seperateByHeader(day.splitlines(),"## ")
+        for e in exercises: 
+            exercise_type = mp.getFirstLine(e,"## ")
+            note = mp.getNote(e)
+            mdTable = mp.getMarkdownTable(e)
+            df = mp.MarkdownToDataframe(mdTable)
+            print(df)
+
+def openMd(path: str):
     try:
         with open(path, 'r', encoding='utf-8') as f:
-            days = mp.seperateByHeader(f.readlines(),"# ")
-            for day in days:
-                 date = mp.getFirstLine(day,"# ")
-                 exercises = mp.seperateByHeader(day.splitlines(),"## ")
-                 for e in exercises: 
-                    exercise_type = mp.getFirstLine(e,"## ")
-                    note = mp.getNote(e)
-                    mdTable = mp.getMarkdownTable(e)
-                    df = mp.MarkdownToDataframe(mdTable)
-                    print(df)
-
+            writeDb(f)
     except FileNotFoundError:
         return []
     except Exception as e:
         return []
 
-    return days
 
 
-myList = extractMD("/home/richard/Documents/Obsidian/Personal/02 Projects/Fitness/Exercise.md")
+myList = openMd("/home/richard/Documents/Obsidian/Personal/02 Projects/Fitness/Exercise.md")
