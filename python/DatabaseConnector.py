42cf6ccc97d75757817d263db01ca2ac501f7777
Richard <57541025+richardh05@users.noreply.github.com>
2025-04-11 17:19:10 +0100

    ExerciseTypeDialogue complete

--- python/DatabaseConnector.py
+++ python/DatabaseConnector.py
@@ -57,9 +57,26 @@ class DatabaseWriter(DatabaseConnector):
         conn.close()
     
 
-    def ExerciseTypeDialogue(exercise:str):
-        print(f"It seems that '{exercise}' doesn't exist in your database. If you'd like to add it, hit enter. Otherwise, type anything else to rename it")
-
+    def ExerciseTypeDialogue(exercise:str) -> Dc.ExerciseType:
+        print(f"It seems that '{exercise}' doesn't exist in your database. If you'd like to add it, hit enter. Otherwise, type anything else to rename it.")
+        user_input = input("> ").strip()
+        xName = str
+        if not user_input:  # User hit enter (empty string)
+            print(f"Adding '{exercise}' to the database.")
+            xName = exercise
+        else:
+            print(f"Renaming '{exercise}' to '{user_input}'.")
+            xName = user_input
+        xUnit = str
+        while not xUnit:
+            print(f"Please enter a unit (Kg, Km, etc) that your exercise will be measured with. If this doesn't apply, enter 'N/A'.")
+            xUnit = input("> ").strip()
+        xCategory = str
+        while not xCategory:
+            print(f"Finally, please specify the category (Push, Pull, Legs or Cardio).")
+            xUnit = input("> ").strip()
+        x = Dc.ExerciseType(xName,xUnit,xCategory)
+        return x
 
     def writeDayClass(self, myDay:Dc.Day):
         conn = self.connect()
@@ -68,6 +85,9 @@ class DatabaseWriter(DatabaseConnector):
         for w in myDay.workouts:
             ExerciseId = self.getIdByUnique("ExerciseType","Name", w.exercise_type)
             if (ExerciseId == None):
-                self.ExerciseTypeDialogue(w.exercise_type)
+                x:Dc.ExerciseType = self.ExerciseTypeDialogue(w.exercise_type)
+                self.WriteExerciseTypeClass(x)
+                ExerciseId = self.getIdByUnique("ExerciseType","Name", w.exercise_type)
+            #self._insert(conn, "Workout", "")
 
 
