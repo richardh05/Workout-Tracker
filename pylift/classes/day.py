0a81e065ec6007a0c34e927a73db0a8ed6675e73
Richard <57541025+richardh05@users.noreply.github.com>
2025-07-18 11:49:42 +0100

    think we're done?

--- pylift/classes/day.py
+++ pylift/classes/day.py
@@ -1,95 +1,100 @@
-from datetime import datetime, date
+from datetime import date, datetime
 import json
 from pathlib import Path
-from typing import List
+
 from pandas import DataFrame
+
 from pylift.classes.exercise_type import ExerciseType
 from pylift.classes.workout import Workout
-from pylift.utils.dialogues import ExerciseTypeDialogue
+from pylift.utils.dialogues import exercise_type_dialogue
 
 
-class Day():
-    def __init__(self, date:date, workouts:List[Workout]):
+class Day:
+    def __init__(self, date: date, workouts: list[Workout]) -> None:
         self.date = date
         self.workouts = workouts
 
-    def __str__(self): return self.date_str
+    def __str__(self) -> str:
+        return self.date_str
 
     @property
     def date_str(self) -> str:
         return self.date.strftime("%Y-%m-%d")
-    
+
     def to_dict(self) -> dict:
         return {
             "date": self.date_str,
-            "workouts": [w.to_dict() for w in self.workouts]
-        }    
-    
+            "workouts": [w.to_dict() for w in self.workouts],
+        }
+
     @classmethod
-    def from_dict(cls, data: dict) -> 'Day':
-        dat = datetime.strptime(data['date'], "%Y-%m-%d").date()
-        workouts = [Workout.from_dict(w) for w in data['workouts']]
+    def from_dict(cls, data: dict) -> "Day":
+        dat = datetime.strptime(data["date"], "%Y-%m-%d").date()
+        workouts = [Workout.from_dict(w) for w in data["workouts"]]
         return cls(date=dat, workouts=workouts)
-    
+
     @classmethod
-    def read(cls, filepath: Path) -> 'Day':
-        with open(filepath, 'r') as f:
-            data:dict = json.load(f)
+    def read(cls, filepath: Path) -> "Day":
+        with filepath.open() as f:
+            data: dict = json.load(f)
         return cls.from_dict(data)
-    
-    def to_dataframe(self, types:List[ExerciseType]) -> DataFrame:
-        data = []
-        for workout in self.workouts:
-            ex = next((et for et in types if et.name == "Squat"))
-            for s in workout.sets:
-                data.append({
-                    "date": self.date_str,
-                    "exercise": workout.exerciseType,
-                    "category": ex.category,
-                    "reps": s.reps,
-                    "value": s.value,
-                    "unit": ex.unit,
-                    "note": workout.note
-                })
+
+    def to_dataframe(self, types: list[ExerciseType]) -> DataFrame:
+        type_map = {et.name: et for et in types}
+        data = [
+            {
+                "date": self.date_str,
+                "exercise": workout.exercise_type,
+                "category": type_map[workout.exercise_type].category,
+                "reps": s.reps,
+                "value": s.value,
+                "unit": type_map[workout.exercise_type].unit,
+                "note": workout.note,
+            }
+            for workout in self.workouts
+            if workout.exercise_type in type_map
+            for s in workout.sets
+        ]
         return DataFrame(data)
-    
+
     @staticmethod
-    def save(d: 'Day', l:List[ExerciseType], filepath: Path) -> None:
-        def ensure_exercise_type_exists(d: Day, l: List[ExerciseType]) -> List[ExerciseType]:
+    def save(d: "Day", exercises: list[ExerciseType], filepath: Path) -> None:
+        def ensure_exercise_type_exists(
+            d: Day, exercises: list[ExerciseType],
+        ) -> list[ExerciseType]:
             for w in d.workouts:
-                if not any(et.name == w.exerciseType for et in l):
-                    e:ExerciseType = ExerciseTypeDialogue(w.exerciseType)
+                if not any(et.name == w.exercise_type for et in exercises):
+                    e: ExerciseType = exercise_type_dialogue(w.exercise_type)
                     ExerciseType.save(e, filepath)
-                    l.append(e)
-            return l
-            
+                    exercises.append(e)
+            return exercises
+
         def merge_day(old: dict, new: dict) -> dict:
-            old_workouts = old['workouts']
-            for new_workout in new['workouts']:
+            old_workouts = old["workouts"]
+            for new_workout in new["workouts"]:
                 # check if this exact workout already exists
                 if not any(new_workout == old_workout for old_workout in old_workouts):
                     old_workouts.append(new_workout)
             return old
-        
-        def write_day(data: dict[str,str|dict], filename: Path) -> None:
+
+        def write_day(data: dict[str, str | dict], filename: Path) -> None:
             if filename.exists():
-                with open(filename, 'r') as f:
+                with filename.open() as f:
                     existing_data = json.load(f)
                 merged_data = merge_day(existing_data, data)
             else:
                 merged_data = data
 
-            with open(filename, 'w') as f:
+            with filename.open("w") as f:
                 json.dump(merged_data, f, indent=4)
 
-        
         filename = filepath / f"{d.date_str}.json"
         data: dict = d.to_dict()
-        l = ensure_exercise_type_exists(d, l)
+        exercises = ensure_exercise_type_exists(d, exercises)
         write_day(data, filename)
 
     @staticmethod
-    def load_days(start:date, end:date, filepath:Path) -> List['Day']:
+    def load_days(start: date, end: date, filepath: Path) -> list["Day"]:
         days = []
         current_date = start
         while current_date <= end:
@@ -99,9 +104,9 @@ class Day():
                 days.append(day)
             current_date = current_date.replace(day=current_date.day + 1)
         return days
-    
+
     @staticmethod
-    def days_to_dataframe(days:List['Day'], types:List[ExerciseType]) -> DataFrame:
+    def days_to_dataframe(days: list["Day"], types: list[ExerciseType]) -> DataFrame:
         data = []
         for day in days:
             df = day.to_dataframe(types)
--- pylift/classes/exercise_type.py
+++ pylift/classes/exercise_type.py
@@ -1,61 +1,59 @@
-from abc import ABC, abstractmethod
-from json import dump, load
-from typing import List
 from dataclasses import dataclass
-from pathlib import Path
-from pandas import DataFrame
 import json
+from json import dump, load
+from pathlib import Path
 
 from pylift.utils.dir import get_data_dir
 
+
 @dataclass
 class ExerciseType:
-    name:str
-    unit:str
-    category:str
+    name: str
+    unit: str
+    category: str
 
     def to_dict(self) -> dict:
         return {
             "name": self.name,
             "unit": self.unit,
-            "category": self.category
+            "category": self.category,
         }
-    
+
     @classmethod
-    def from_dict(cls, data: dict) -> 'ExerciseType':
+    def from_dict(cls, data: dict) -> "ExerciseType":
         return ExerciseType(
-            name=data['name'],
-            unit=data['unit'],
-            category=data['category']
+            name=data["name"],
+            unit=data["unit"],
+            category=data["category"],
         )
-    
+
     @classmethod
-    def read(cls, filepath: Path) -> List['ExerciseType']:
+    def read(cls, filepath: Path) -> list["ExerciseType"]:
         """
         Read a list of ExerciseType from a JSON file.
         """
-        with open(filepath, 'r') as f:
-            data:List[dict] = json.load(f)
+        with filepath.open() as f:
+            data: list[dict] = json.load(f)
         return [cls.from_dict(et) for et in data]
 
     @staticmethod
-    def save(et: 'ExerciseType', filepath: Path=get_data_dir()) -> None:
+    def save(et: "ExerciseType", filepath: Path = get_data_dir()) -> None:  # noqa: B008
         """
         Save a single ExerciseType to a JSON file. Merges with existing ExerciseType data if it already exists.
         """
         filename = filepath / "exercises.json"
-        newdata:dict[str,str] = et.to_dict()
-        with open(filename, 'rw') as f:
+        newdata: dict[str, str] = et.to_dict()
+        with filename.open("w") as f:
             if not filename.exists():
                 dump({"ExerciseTypes": [newdata]}, f, indent=4)
                 return
-            existing_data:List[dict[str,str]] = load(f)
+            existing_data: list[dict[str, str]] = load(f)
             # Check if the exercise type already exists
-            if not any(et['name'] == newdata['name'] for et in existing_data):
+            if not any(et["name"] == newdata["name"] for et in existing_data):
                 existing_data.append(newdata)
             else:
                 # Update the existing exercise type if it exists
                 for i, existing_et in enumerate(existing_data):
-                    if existing_et['name'] == newdata['name']:
+                    if existing_et["name"] == newdata["name"]:
                         existing_data[i] = newdata
                         break
