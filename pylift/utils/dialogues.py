26762e404afcf8701bd3b294daf213ada97f7da8
Richard <57541025+richardh05@users.noreply.github.com>
2025-07-20 21:35:44 +0100

    ruff formatting new tests

--- pylift/utils/dialogues.py
+++ pylift/utils/dialogues.py
@@ -3,7 +3,7 @@ from pylift.classes.exercise_type import ExerciseType
 
 def exercise_type_dialogue(exercise: str) -> ExerciseType:
     print(
-        f"It seems that '{exercise}' doesn't exist in your database. If you'd like to add it, hit enter. Otherwise, type anything else to rename it."
+        f"It seems that '{exercise}' doesn't exist in your database. If you'd like to add it, hit enter. Otherwise, type anything else to rename it.",
     )
     user_input = input("> ").strip()
     x_name: str
@@ -16,7 +16,7 @@ def exercise_type_dialogue(exercise: str) -> ExerciseType:
     x_unit: str = ""
     while not x_unit:
         print(
-            "Please enter a unit (Kg, Km, etc) that your exercise will be measured with. If this doesn't apply, enter 'N/A'."
+            "Please enter a unit (Kg, Km, etc) that your exercise will be measured with. If this doesn't apply, enter 'N/A'.",
         )
         x_unit = input("> ").strip()
     x_category: str = ""
--- tests/test_input_markdown.py
+++ tests/test_input_markdown.py
@@ -1,42 +1,44 @@
 from pathlib import Path
-from typing import List
+
 import pytest
-from tests.factories.day_factory import DayFactory
-from pylift.input.markdown import read_markdown
+
 from pylift.classes.day import Day
-from pylift.classes.workout import Workout
-from pylift.classes.set import Set
-from datetime import date
+from pylift.input.markdown import read_markdown
+from tests.factories.day_factory import DayFactory
 
 seeds = [42, 123, 456]
 
-def cases(seeds:list[int]) -> list[tuple[str, List[Day]]]:
-  fact = DayFactory(seeds[0])
-  l = [(fact.static_markdown(), fact.static_days())]
-  for seed in seeds[1:]:
-    fact = DayFactory(seed)
-    r = fact.random_markdown()
-    l.append(r)
-  return l
+
+def cases(seeds: list[int]) -> list[tuple[str, list[Day]]]:
+    factory = DayFactory(seeds[0])
+    comparisons = [(factory.static_markdown(), factory.static_days())]
+    for seed in seeds[1:]:
+        factory = DayFactory(seed)
+        r = factory.random_markdown()
+        comparisons.append(r)
+    return comparisons
+
 
 @pytest.mark.parametrize("markdown, expected_days", cases(seeds))
-def test_read_markdown(markdown, expected_days, tmp_path):
-  # Write the markdown to a temporary file
-  with open(tmp_path / "test_markdown.md", "w", encoding="utf-8") as f:
-    f.write(markdown)
+def test_read_markdown(markdown, expected_days, tmp_path: Path) -> None:
+    # Write the markdown to a temporary file
+    with open(tmp_path / "test_markdown.md", "w", encoding="utf-8") as f:
+        f.write(markdown)
 
-  # Read the markdown file
-  md_days = read_markdown(Path( tmp_path / "test_markdown.md"))
+    # Read the markdown file
+    md_days = read_markdown(Path(tmp_path / "test_markdown.md"))
 
-  for i in range(len(md_days)):
-    day = expected_days[i]
-    md_day = md_days[i]
-    assert isinstance(md_day, Day), f"Expected Day instance, got {type(md_day)}"
-    assert md_day.date == day.date, f"Expected date {day.date}, got {md_day.date}"
-    assert len(md_day.workouts) == len(day.workouts), f"Expected {len(day.workouts)} workouts, got {len(md_day.workouts)}"
-    for j in range(len(md_day.workouts)):
-      assert md_day.workouts[j].exerciseType == day.workouts[j].exerciseType
-      assert md_day.workouts[j].note == day.workouts[j].note
-      assert len(md_day.workouts[j].sets) == len(day.workouts[j].sets)
-      for k in range(len(md_day.workouts[j].sets)):
-        assert md_day.workouts[j].sets[k] == day.workouts[j].sets[k]
+    for i in range(len(md_days)):
+        day = expected_days[i]
+        md_day = md_days[i]
+        assert isinstance(md_day, Day), f"Expected Day instance, got {type(md_day)}"
+        assert md_day.date == day.date, f"Expected date {day.date}, got {md_day.date}"
+        assert len(md_day.workouts) == len(day.workouts), (
+            f"Expected {len(day.workouts)} workouts, got {len(md_day.workouts)}"
+        )
+        for j in range(len(md_day.workouts)):
+            assert md_day.workouts[j].exercise_type == day.workouts[j].exercise_type
+            assert md_day.workouts[j].note == day.workouts[j].note
+            assert len(md_day.workouts[j].sets) == len(day.workouts[j].sets)
+            for k in range(len(md_day.workouts[j].sets)):
+                assert md_day.workouts[j].sets[k] == day.workouts[j].sets[k]
