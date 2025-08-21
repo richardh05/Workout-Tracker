bf8e35b372cd232fc66ed6453876a2a683de68fc
Richard <57541025+richardh05@users.noreply.github.com>
2025-08-21 15:35:17 +0100

    is this better?

--- pylift/input/helper.py
+++ pylift/input/helper.py
@@ -1,10 +1,8 @@
-from pandas import DataFrame
-
 class InputHelper:
   @property
   def aliases(self) -> dict[str, list[str]]:
     aliases = {
-        # 'day': ['d', 'days'],
+        'day': ['days'],
         'exercise': ['exercises'],
         'set': ['sets'],
         'workout': ['workouts'],
@@ -13,37 +11,10 @@ class InputHelper:
         'value': ['weight', 'wgt'],
         'note': ['notes'],
         'date': ['dte', 'dates', "day", "days"],
-        'unit': ['units']
+        'unit': ['units'],
     }
-    for key in aliases:
-          aliases[key] = [key] + aliases[key]
+    for key, values in aliases.items():
+          aliases[key] = [key, *values]
 
     return aliases
-  
-  def sanitize_col_names(self , df: DataFrame) -> DataFrame:
-      
 
-  def sanitize_units(self, df: DataFrame) -> DataFrame:
-    def sanitize_unit_rows(
-            values: list[str],
-            unit_labels: list[str],
-            unit_cols: list[list[str]]
-
-    ) -> tuple[list[float],str]:
-        
-    unit_aliases = self.aliases['unit']
-    unit_cols = []
-
-    # Loop over each alias and each column name directly
-    for alias in unit_aliases:
-        for col in df.columns:
-            if alias in col:
-                unit_cols.append(col)
-
-
-      if unit_cols:
-          print(f"Found unit column: {unit_cols}")
-      else:
-          print("No unit column found.")
-  
-  
